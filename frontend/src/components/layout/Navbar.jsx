748f4b2689b321f20080714db5c8b9f66d420782
Karthik D <d.karthikdhanasaker2021@gmail.com>
2026-05-06 14:13:27 +0530

    Update Navbar.jsx

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link, useNavigate, useLocation } from 'react-router-dom';
 import { Menu, X, HeartPulse, UserCircle } from 'lucide-react';
 import { useState, useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import { BASE_URL } from '../../config';
+import { BASE_URL } from '../../config.js';
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
     const [user, setUser] = useState(null);
