e98a1d46550cb2fdca7ec7befe5187e26e65e5db
Karthik D <d.karthikdhanasaker2021@gmail.com>
2026-05-08 17:51:16 +0530

    Update Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -3,7 +3,8 @@ import { motion } from 'framer-motion';
 import { Link, useNavigate } from 'react-router-dom';
 import { Eye, EyeOff, UserPlus, HeartPulse, CheckCircle } from 'lucide-react';
 
-import API_URL from '../config.js';
+
+import API_URL from '@/config.js';
 
 const Register = () => {
     const [formData, setFormData] = useState({ username: '', email: '', password: '', confirmPassword: '', role: 'user' });
